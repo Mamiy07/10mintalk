2558d0caf0e883e44f730518eef87ca6ff068351
Mamiy07 <mamiy7469@gmail.com>
2026-01-30 17:37:05 +0530

    completed

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -8,7 +8,18 @@ import { nanoid } from 'nanoid'
 
 
 const rooms = new Elysia({prefix:'/room'}).
-post('/create', createRoom)
+post('/create', createRoom).use(authMiddleware).get('/ttl',async ({auth})=>{
+   const ttl = await redis.ttl(`meta:${auth.roomId}`)
+   return {ttl:ttl>0 ? ttl : 0}
+},{ query:z.object({   roomId: z.string() })}).delete("/",async ({auth})=>{
+   
+    await realtime.channel(auth.roomId).emit('chat.destroy',{isDestroyed:true})
+   await Promise.all([
+      redis.del(auth.roomId),
+      redis.del(`meta:${auth.roomId}`),
+      redis.del(`messages:${auth.roomId}`)
+   ])
+},{ query:z.object({ roomId: z.string()})})
 
 const messages = new Elysia({prefix:'/messages'}).use(authMiddleware).post('/',async ({body , auth})=>{
     const {sender , text} = body 
@@ -32,7 +43,6 @@ const roomExists = await redis.exists(`meta:${auth.roomId}`)
    
     const remaning = await redis.ttl(`meta:${auth.roomId}`)
     await redis.expire(`messages:${auth.roomId}`, remaning)
-    await redis.expire(`history:${auth.roomId}`, remaning)
     await redis.expire(auth.roomId, remaning)
 
   
@@ -41,12 +51,18 @@ const roomExists = await redis.exists(`meta:${auth.roomId}`)
       sender: z.string().max(100),
       text: z.string().max(1000)
 }) }
-    )
+    ).get('/',async({auth})=>{
+        const messages = await redis.lrange<Message>(`messages:${auth.roomId}`,0,-1)
+        return {messages:messages.map((m)=>({
+            ...m,
+            token:m.token === auth.token ? auth.token : undefined
+        }))}
+    },{ query:z.object({ roomId: z.string()})})
 
 const app = new Elysia({ prefix: '/api' }).use(rooms).use(messages)
  
     
-
+export const DELETE = app.fetch
 export const GET = app.fetch 
 export const POST = app.fetch 
 export type App = typeof app
--- src/app/room/[roomId]/page.tsx
+++ src/app/room/[roomId]/page.tsx
@@ -3,18 +3,62 @@
 import { useUsername } from "@/hooks/use-username";
 import { client } from "@/lib/client";
 import { formatTimeRemaining } from "@/utils/room";
-import { useMutation } from "@tanstack/react-query";
-import { useParams } from "next/navigation";
-import { useRef, useState } from "react";
-
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import { format } from 'date-fns';
+import { useRealtime } from "@/lib/realtime-client";
 const Page = () => {
   const { username } = useUsername();
+  const router = useRouter()
   const [copy, setCopy] = useState("COPY");
   const [timeRemaining, setTimeRemaining] = useState<number | null>(null);
+  
   const [input, setInput] = useState("");
   const inputRef = useRef<HTMLInputElement>(null);
   const params = useParams();
   const roomId = params.roomId as string;
+ 
+  const {data:ttlData} = useQuery({
+    queryKey:['ttl',roomId],
+    queryFn:async ()=>{
+      const res = await client.room.ttl.get({query:{roomId}})
+      return res.data
+    }
+  })
+
+  useEffect(()=>{
+      if(ttlData?.ttl !== undefined){
+        setTimeRemaining(ttlData.ttl)
+      }
+  },[ttlData])
+
+  useEffect(()=>{
+    if(timeRemaining === null || timeRemaining <=0) return;
+    if(timeRemaining === 0){
+      router.push('/?destroyed=true' )
+      return
+    }
+    const interval = setInterval(()=>{
+      setTimeRemaining((prev)=>{
+        if(prev === null || prev <=1){
+          clearInterval(interval)
+          return 0
+        }
+        return prev -1
+      })
+    },1000)
+    return ()=> clearInterval(interval)
+  },[timeRemaining,router])
+
+  const {data:messages,refetch}=useQuery({
+    queryKey:['messages',roomId],
+    queryFn:async ()=>{
+      const res = await client.messages.get({query:{roomId}})
+      return res.data
+    }
+  })
+
   const { mutate: sendMessage,isPending } = useMutation({
     mutationFn: async ({ text }: { text: string }) => {
       await client.messages.post(
@@ -23,9 +67,30 @@ const Page = () => {
           text,
         },
         { query: { roomId } },
+
       );
+      setInput("");
     },
   });
+  const { mutate: destroyRoom } = useMutation({
+    mutationFn: async () => {
+      await client.room.delete(null,{ query: { roomId } });
+    }
+  })
+
+  useRealtime({
+      channels:[roomId],
+      events:['chat.message','chat.destroy'],
+      onData:({event})=>{
+        if(event==='chat.message'){
+           refetch()
+        }
+        if(event==='chat.destroy'){
+          router.push('/?destroyed=true' )
+        }
+      }
+    })
+
   const copyLink = () => {
     const url = window.location.href;
     navigator.clipboard.writeText(url);
@@ -66,12 +131,28 @@ const Page = () => {
           </div>
         </div>
 
-        <button className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all flex items-center gap-2 disabled:opacity-50">
+        <button onClick={()=>destroyRoom()} className="text-xs bg-zinc-800 hover:bg-red-600 px-3 py-1.5 rounded text-zinc-400 hover:text-white font-bold transition-all flex items-center gap-2 disabled:opacity-50">
           {" "}
           <span className="group-hover:animate-pulse">💣</span> DESTROY NOW
         </button>
       </header>
-      <div className="flex-1 overflow-y-hidden p-4"></div>
+
+      <div className="flex-1 overflow-y-hidden p-4 space-y-4 scrollbar-thin">
+        {messages?.messages.length === 0 && ( <div className="flex items-center justify-center h-full">
+          <p className="text-zinc-500 text-sm font-mono ">No messages yet, Start the conversation</p>
+        </div> )}
+        {messages?.messages.map((msg)=>(
+        <div key={msg.id} className="flex flex-col items-start">
+          <div className="max-w-[80%] group">
+            <div className="flex items-baseline gap-3 mb-1">
+              <span className={`text-xs font-bold ${msg.sender === username  ? 'text-green-500':'text-blue-500'}`}>{msg.sender === username ? "You" : msg.sender}</span>
+              <span className="text-[10px] text-zinc-600">{ format(msg.timestamp,"HH:mm")}</span>
+            </div>
+            <p className="text-sm text-zinc-300 leading-relaxed break-all">{msg.text}</p>
+          </div>
+        </div>
+       ))}
+      </div>
       <div className="p-4 border-t border-zinc-800 bg-zinc-900/30">
         <div className="flex gap-4">
           <div className="flex-1 relative group">
@@ -86,6 +167,7 @@ const Page = () => {
                   //SEND MESSAGE TO BE
                   sendMessage({ text: input.trim() });
                   inputRef.current?.focus();
+                  setInput("");
                 }
               }}
               autoFocus
@@ -98,6 +180,7 @@ const Page = () => {
             onClick={() => {
               sendMessage({ text: input});
               inputRef.current?.focus();
+              setInput("");
             }}
             disabled={!input.trim() || isPending}
             className="bg-zinc-800 text-zinc-400 px-6 text-sm font-bold transition-all hover:text-zinc-200 disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
